647f7549829918785a490dbb40247180c18e1193
Amith <kondaamithsai8@gmail.com>
2022-05-01 15:51:44 +0530

    state and effect

--- frontend/src/displays/HomeDisplay.js
+++ frontend/src/displays/HomeDisplay.js
@@ -1,12 +1,22 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import data from '../data';
+// import data from '../data';
+import axios from 'axios';
 
 function HomeDisplay() {
+  const [artists, setArtists] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get('/api/artists');
+      setArtists(result.data);
+    };
+    fetchData();
+  }, []);
   return (
     <div>
       <h1>Artists</h1>
       <div className="arts">
-        {data.artists.map((artist) => (
+        {artists.map((artist) => (
           <div className="art" key={artist.artist_id}>
             <Link to={`/art/${artist.artist_id}`}>
               <img src={artist.artist_img} alt={artist.artist}></img>
