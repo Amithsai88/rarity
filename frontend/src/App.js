1655d6df5f1d767dd060ff69771e39de8a81fd3f
Amith <kondaamithsai8@gmail.com>
2022-05-03 00:19:36 +0530

    added bootstrap styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,30 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomeDisplay from './displays/HomeDisplay';
 import Arts from './displays/Arts';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
+      <div className="App d-flex flex-column">
         <header>
-          <a href="/">RareBuy</a>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>RareBuy</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
         </header>
         <main>
-          <Routes>
-            <Route path="/art/:artist_id" element={<Arts />} />
-            <Route path="/" element={<HomeDisplay />} />
-          </Routes>
+          <Container>
+            <Routes>
+              <Route path="/art/:artist_id" element={<Arts />} />
+              <Route path="/" element={<HomeDisplay />} />
+            </Routes>
+          </Container>
         </main>
       </div>
     </BrowserRouter>
