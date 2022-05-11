5e1e81cbdc4155ba7078d26e0a22a7e205e53a3e
Amith <kondaamithsai8@gmail.com>
2022-05-12 02:09:40 +0530

    added alert in signin

--- frontend/src/displays/Signin.js
+++ frontend/src/displays/Signin.js
@@ -1,15 +1,37 @@
 import Container from 'react-bootstrap/esm/Container';
 import Form from 'react-bootstrap/esm/Form';
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { useContext, useState } from 'react';
 import { Helmet } from 'react-helmet-async';
 import Button from 'react-bootstrap/esm/Button';
+import Axios from 'axios';
+import { Store } from '../Store.js';
 
 export default function Signin() {
+  const navigate = useNavigate();
   const { search } = useLocation();
   const redirectInUrl = new URLSearchParams(search).get('redirect');
   const redirect = redirectInUrl ? redirectInUrl : '/';
 
-  const submitHandler = async (e) => {};
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await Axios.post('/api/users/signin', {
+        email,
+        password,
+      });
+      ctxDispatch({ type: 'USER_SIGNIN', payload: data });
+      localStorage.setItem('userInfo', JSON.stringify(data));
+      navigate(redirect || '/');
+    } catch (err) {
+      alert('Invalid Email or Password');
+    }
+  };
 
   return (
     <Container className="small-container">
@@ -20,11 +42,19 @@ export default function Signin() {
       <Form onSubmit={submitHandler}>
         <Form.Group className="mb-3" controlId="email">
           <Form.Label>Email</Form.Label>
-          <Form.Control type="email" required />
+          <Form.Control
+            type="email"
+            required
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </Form.Group>
         <Form.Group className="mb-3" controlId="password">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password " required />
+          <Form.Control
+            type="password "
+            required
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </Form.Group>
         <div className="mb-3">
           <Button type="submit">Sign In</Button>
