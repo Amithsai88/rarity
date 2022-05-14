8a40392a3e52c21226c0f72e3251cb4d79241e66
Amithsai88 <44061989+Amithsai88@users.noreply.github.com>
2022-05-14 17:59:48 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -25,15 +25,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/api/seed', seedRouter);
 app.use('/api/users', userRouter);
 
-// const __dirname = path.resolve();
-// app.use(express.static(path.join(__dirname, '/frontend/build')));
-// app.get('*', (req, res) =>
-//   res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
-// );
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
+);
 
-app.get('/api/artists', (req, res) => {
-  res.send(data.artists);
-});
+// app.get('/api/artists', (req, res) => {
+//   res.send(data.artists);
+// });
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
